b1eabad78b5cb7c8b93225dc670e9e96887e8692
Liam Grossmann <liam.grossmann@protonmail.com>
2022-10-02 12:47:19 +0200

    Add star wars css, texts and animations

--- src/index.js
+++ src/index.js
@@ -2,7 +2,37 @@ import _ from 'lodash';
 import * as d3 from 'd3';
 import printMe from './print.js';
 import './css/normalize.css';
-import './css/style.css';
+import './css/starwars.css';
+
+
+function doAnimations() {
+  let introductionTextDisappears = 2000;
+  let logoTextAppears = introductionTextDisappears + 250;
+  let logoTextFadeOut = logoTextAppears + 500;
+  let logoTextFadeOutDuration = 2500;
+  let scrollDuration = 100;
+  let oneMinute = 50000;
+
+  const introductionTextElement = document.getElementById('introductionText');
+  const logoElement = document.getElementById('logo');
+  const titlecontentElement = document.getElementById('titlecontent');
+
+  // Apply class so that the scrolling text will scroll. Change color so it is visible.
+  // TODO: Remove jQuery reference!!
+  titlecontentElement.classList.add('scrollingText');
+  titlecontentElement.style.color = '#ff6';
+
+  // Intro text will disappear
+  d3.select(introductionTextElement).transition().delay(introductionTextDisappears).style('opacity', '0');
+
+  // Image will get smaller and disappear.
+  d3.select(logoElement).transition().delay(logoTextAppears).style('opacity', '1');
+  d3.select(logoElement).transition().delay(logoTextFadeOut)
+    .duration(logoTextFadeOutDuration)
+    .style('font-size', '5px').style('opacity', '0');
+};
+
+
 
 function component() {
   const element = document.createElement('div');
@@ -33,5 +63,7 @@ function d3Paras() {
   return element;
 }
   
-document.body.appendChild(component());
-document.body.appendChild(d3Paras());
+//document.body.appendChild(component());
+//document.body.appendChild(d3Paras());
+
+doAnimations();
